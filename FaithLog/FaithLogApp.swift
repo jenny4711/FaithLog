ceb4e40ed3afdc20f5067b6ed3a87907e25f28fc
jiylee <jjenny4711@gmail.com>
2025-07-30 01:13:58 -0400

    Initial Commit

---
+++ FaithLog/FaithLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaithLogApp.swift
+//  FaithLog
+//
+//  Created by Ji y LEE on 7/30/25.
+//
+
+import SwiftUI
+
+@main
+struct FaithLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
